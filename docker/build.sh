a7253b091586e31ababcb2f2f8cc83627351442e
Sébastien Mosser <sebastien.mosser@gmail.com>
2019-10-21 12:47:29 -0400

    image docker / reproductible

---
+++ docker/build.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+docker build -t "petitroll/island" .
---
+++ docker/run.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+docker run -it -v "/Users/mosser/work/teaching/A19-INF-5153/championship/deliveries:/island" petitroll/island
